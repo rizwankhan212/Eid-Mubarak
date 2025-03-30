5ca707af033cc82383489dd7ee580e2f44e5041a
Rizwan Khan <151928859+rizwankhan212@users.noreply.github.com>
2025-03-30 23:02:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ function showGreeting() {
     if (name.trim() === "") {
         document.getElementById("greeting").innerHTML = "Please enter your name!";
     } else {
-        document.getElementById("greeting").innerHTML = `Wishing you and your family a blessed Eid ul-Fitr, ${name}. from Rizwan. 🎉!`;
+        document.getElementById("greeting").innerHTML = `<b>${name}</b> ke ghar aaye Eid ki khushiyon ki sawari, <b>${name}</b> ki dua ho kabul har dafa sari, <b>${name}</b> ki Eid ho sabse nirali.`
+
+     document.getElementById("me").innerHTML = "Aapko aur Aapki puri family ko <b>Rizwan</b> ki pure Family ki Tarf se <b>Eid ul Fitr Mubarak</b> 🎉!";
     }
 }
